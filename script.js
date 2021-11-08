d937ebb0548be89d5502603ec3c201c1471b168b
tuffany <thetifftopor@gmail.com>
2021-11-08 15:24:10 -0500

    little makeover

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+window.addEventListener('load', () => {
+        // noinspection JSUnresolvedVariable
+        let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'audio-autoplay.wav');
+        xhr.responseType = 'arraybuffer';
+        xhr.addEventListener('load', () => {
+          let playsound = (audioBuffer) => {
+            let source = audioCtx.createBufferSource();
+            source.buffer = audioBuffer;
+            source.connect(audioCtx.destination);
+            source.loop = true;
+            source.start();
+
+          }
+
+          //   setTimeout(function () {
+          //     let t = document.createElement('p');
+          //     t.appendChild(document.createTextNode((new Date()).toLocaleString() + ': Sound played'));
+          //     document.querySelector('.output').appendChild(t);
+          //     playsound(audioBuffer);
+          //   }, 1000 + Math.random()*2500);
+          // };
+
+        //   audioCtx.decodeAudioData(xhr.response).then(playsound);
+        // });
+      //   xhr.send();
+      // });
